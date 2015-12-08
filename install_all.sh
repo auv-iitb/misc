e2e94d728791bd9bf8edcdf95b994949d8c996b4
Kunal Tyagi <tyagi.kunal@live.com>
2015-12-09 01:11:15 +0530

    Create install_all.sh

---
+++ install_all.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# run this setup with the following command
+# curl --silent <addr> | bash
+
+# Setup sources list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+# Setup keys
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+# Install
+
+bash ./install_deps.sh
+
+# Initialize rosdep
+sudo rosdep init
+rosdep update
+sudo apt-get -y install python-rosinstall </dev/null
+
+# Environment setup
+source /opt/ros/indigo/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+
+printf "Adding required files to ~/.bashrc\n"
+echo 'source '`rospack find auv_utils`'/src/utility/bash/robosub_aliases' >> ~/.bashrc
+echo 'source '`rospack find auv_utils`'/src/utility/bash/robosub_bash' >> ~/.bashrc
+
+printf "Getting the latest robosub repository"
+cd ~/catkin_ws/src
+git clone http://github.com/auv-iitb/robosub.git
+cd ~/catkin_ws/
+catkin_make
