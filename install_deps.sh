14f4402e2dcf192a400ff74f437da37d700b4b4b
Kunal Tyagi <tyagi.kunal@live.com>
2015-12-09 01:12:23 +0530

    Create install_deps.sh

---
+++ install_deps.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+printf "Updating your package repositories (Will take a few minutes....)\n"
+sudo apt-get -y update >/dev/null </dev/null
+
+printf "Installing Serial communication library\n"
+sudo apt-get -y install libserial-dev >/dev/null </dev/null
+
+printf "Installing AVR tools\n"
+sudo apt-get -y install avr-libc avrdude gcc-avr >/dev/null </dev/null
+
+printf "Installing coriander\n"
+sudo apt-get -y install coriander >/dev/null </dev/null
+
+printf "Installing necessary ROS packages\n"
+sudo apt-get -y install ros-indigo-desktop-full ros-indigo-uvc-camera ros-indigo-camera1394 ros-indigo-camera-umd </dev/null
+#long install time
+
+printf "Installing necessary Gazebo related packages\n"
+bash ./install_gazebo.sh
+
+printf "Installing flex and bison for parser\n"
+sudo apt-get -y install flex bison >/dev/null </dev/null
+
+printf "Installing google testing environment\n"
+sudo apt-get install libgtest-dev google-mock ros-indigo-rostest </dev/null
+
+printf "Installing additional usefull programs\n"
+sudo apt-get -y install cutecom tree cloc vim git guake terminator tilda python-pip openssh-server >/dev/null </dev/null
+
+printf "Installing usefull python packages\n"
+sudo pip install -U numpy scipy matplotlib pyqtgraph autocomplete </dev/null
